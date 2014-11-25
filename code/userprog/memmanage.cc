652c69ebe63c66789863ec2e774e0df4ac60a7ee
trtucker <trtucker@ucsd.edu>
2014-11-25 10:41:52 -0800

    Updated memory manager to use awful pointers due to compiler complaints.

--- code/userprog/memmanage.cc
+++ code/userprog/memmanage.cc
@@ -3,28 +3,33 @@
 
 MemoryManager::MemoryManager(int numPages)
 {
-  mNumPages  numPages;
-  bmap = BitMap(numPages);
+  mNumPages = numPages;
+  bmap = new BitMap(numPages);
 }
 
 int MemoryManager::AllocPage()
 {
   for (int i = 0; i < mNumPages; ++i) {
-    if (!bmap.Test(i)) {
-      bmap.Mark(i);
+    if (!bmap->Test(i)) {
+      bmap->Mark(i);
       return i;
     }
   }
   return -1;
 }
 
+MemoryManager::~MemoryManager()
+{
+  delete bmap;
+}
+
 void MemoryManager::FreePage(int physPageNum)
 {
-  bmap.Clear(physPageNum);
+  bmap->Clear(physPageNum);
 }
 
 bool MemoryManager::PageIsAllocated(int physPageNum)
 {
-  return bmap.Test(physPageNum);
+  return bmap->Test(physPageNum);
 }
 
--- code/userprog/memmanage.h
+++ code/userprog/memmanage.h
@@ -4,17 +4,20 @@
 // maco guard ftw
 #define MEMMANAGE_H
 
+#include "bitmap.h"
+
 class MemoryManager {
 
   private:
     int mNumPages;
-    BitMap bmap(0);
+    BitMap * bmap;
 
   public:
     /* Create a manager to track the allocation of numPages of physical memory.  
        You will create one by calling the constructor with NumPhysPages as
        the parameter.  All physical pages start as free, unallocated pages. */
     MemoryManager(int numPages);
+    ~MemoryManager();
 
     /* Allocate a free page, returning its physical page number or -1
        if there are no free pages available. */
